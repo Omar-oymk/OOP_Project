6bb4f0b2deab238dc40dc16f4ce942e0b60cb70b
omaroymk <72468295+oymk0@users.noreply.github.com>
2025-05-24 00:04:39 +0300

    Created a Count.cpp file that counts to 10

---
+++ count.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void countToTen();
+
+int main()
+{
+    countToTen();
+}   
+
+void countToTen()
+{
+    for (int i = 1; i < 11; i++)
+    {
+        cout<<i<<" ";
+    }
+    cout<<endl;
+}
